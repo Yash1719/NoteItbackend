6d2e9165d45a936835a5e92f01b9c28d62e5c135
Yash1719 <76112649+Yash1719@users.noreply.github.com>
2024-08-28 01:00:59 +0530

    Update authenticator.js

--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 function authenticator(req,res,next){
     const token = req.headers.authorization
-    jwt.verify(token,"saurabh",(err,decode)=>{
+    jwt.verify(token,process.env.SECRET_KEY,(err,decode)=>{
         if(err) return  res.send({
             message:"Token is not valid please login",
             status:2
